a72aa62cca1d5e348fd23291d6691e7b29864f9f
Noya Onggud <yanuo44@gmail.com>
2021-12-01 15:14:52 -0500

    implemented word search function and tests

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,9 +1,9 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
-describe("#wordSearch()", function() {
+describe("#wordSearch()", function()    {
   it("should return false if the word is not present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
@@ -15,7 +15,7 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
@@ -31,8 +31,24 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
-
+    ], 'SEINFELD');
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['D', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['A', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['M', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'ADAM');
+    assert.isTrue(result);
+  });
+
+
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,43 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  let result = false;
+  const horizontalJoin = letters.map(ls => {
+    return ls.join('');
+  });
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) {
+      result = true;
     }
-}
+    // else
+    // {
+    //   result = false;
+    // }
+  }
+  const verticalJoin = transpose(letters).map(ls => {
+    return ls.join('');
+  });
+  for (const l of verticalJoin) {
+    if (l.includes(word)) {
+      result = true;
+    }
+    // else {
+    //   result = false;
+    // }
+  }
+  
+  return result;
+};
 
-module.exports = wordSearch
+
+const transpose = function(matrix) {
+  let output = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    let tempOutput = [];
+    for (let j = 0; j < matrix.length; j++) {
+      tempOutput.push(matrix[j][i]);
+    }
+    output.push(tempOutput);
+  }
+  return output;
+};
+
+module.exports = wordSearch;
